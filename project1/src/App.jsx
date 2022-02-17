fd5cb6f5196b7018a7eb902e163db0fb8acbce15
Sahil Rajput <sahilrajput03@gmail.com>
2022-02-18 00:23:51 +0530

    Add day1 components.

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,43 +1,18 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import * as Day1 from './components/Day1'
+
+console.log(Day1)
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+	  <h1>This is App component</h1>
+	  <Day1.Component1/>
+	  <Day1.Component2/>
+	  <Day1.Component3/>
     </div>
   )
 }
