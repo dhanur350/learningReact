803a019fbb2d3cdb48b8f1108beb63f0c544aece
dhanur350 <dhanur297@gmail.com>
2023-01-22 13:26:15 +0530

    Day7

--- reactprj/src/App.jsx
+++ reactprj/src/App.jsx
@@ -1,4 +1,11 @@
 import React, { Fragment } from "react";
+import {Article,Brand,Cta,Feature,Navbar} from "./components";
+/*import Article from "./components/article/Article";
+import Brand from "./components/brand/Brand"
+import Cta from "./components/cta/Cta"
+import Feature from "./components/feature/Feature"
+import Navbar from "./components/navbar/Navbar"
+*/
 
 const App = ()=>{
     return(
