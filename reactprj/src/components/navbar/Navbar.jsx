bbdfef2f4bc07ffe9f7231f9c88152b85a1cc841
dhanur350 <dhanur297@gmail.com>
2023-02-04 23:37:20 +0530

    Day10.1

--- reactprj/src/components/navbar/Navbar.jsx
+++ reactprj/src/components/navbar/Navbar.jsx
@@ -10,6 +10,14 @@ function Navbar() {
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
         </div>
+        <div className="gpt3__navbar-links-container">
+          <p><a href="#home">Home</a></p>
+          <p><a href=""></a></p>
+          <p><a href=""></a></p>
+          <p><a href=""></a></p>
+          <p><a href=""></a></p>
+          <p><a href=""></a></p>
+        </div>
       </div>
     </div>
   )
