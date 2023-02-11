f08b91f0ccee8307dfe9307f135590af2b1b6a58
dhanur350 <dhanur297@gmail.com>
2023-02-11 19:38:43 +0530

    Day5.3

--- reactprj/src/components/navbar/Navbar.jsx
+++ reactprj/src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {RiMenu3Line, RiCloseLin} from "react-icons/ri"
+import {RiMenu3Line, RiCloseLine} from "react-icons/ri"
 import logo from "../../assets/logo.svg"
 import "./navbar.css"
 
