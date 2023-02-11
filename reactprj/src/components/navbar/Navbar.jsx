79fb0ed286beb28936dc3bb017fdf349450ac320
dhanur350 <dhanur297@gmail.com>
2023-02-11 22:47:03 +0530

    Day5.5

--- reactprj/src/components/navbar/Navbar.jsx
+++ reactprj/src/components/navbar/Navbar.jsx
@@ -13,9 +13,9 @@ function Navbar() {
         <div className="gpt3__navbar-links-container">
           <p><a href="#home">Home</a></p>
           <p><a href="#wgpt3">What is GPT?</a></p>
-          <p><a href="#possibility">Possibility</a></p>
-          <p><a href="#features">Features</a></p>
-          <p><a href="#blog">Blog</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
         </div>
       </div>
       <div className="gpt3__navbar-sign">
