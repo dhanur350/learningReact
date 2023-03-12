5e96b04182c363686f76b8fb7ddf9c9578370b00
dhanur350 <dhanur297@gmail.com>
2023-03-13 00:15:49 +0530

    Day8.0.1

--- reactprj/src/components/navbar/Navbar.jsx
+++ reactprj/src/components/navbar/Navbar.jsx
@@ -3,14 +3,16 @@ import {RiMenu3Line, RiCloseLine} from "react-icons/ri"
 import logo from "../../assets/logo.svg"
 import "./navbar.css"
 
-function Menu(){
-  <Fragment>
+function Menu (){
+  return(
+    <Fragment>
     <p><a href="#home">Home</a></p>
           <p><a href="#wgpt3">What is GPT?</a></p>
           <p><a href="#possibility">Open AI</a></p>
           <p><a href="#features">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>
   </Fragment>
+  )
 }
 
 function Navbar() {
